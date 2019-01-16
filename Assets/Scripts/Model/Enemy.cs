2b3af810ef7186b750853b5b58538fbd5773710d
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-01-16 14:42:13 +0700

    add behaviour for enemies

---
+++ Assets/Scripts/Model/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject balloon;
+
+    public UnityAction OnBallonDestroyed = delegate { };
+    public UnityAction OnEnemyKilled = delegate { };
+
+    private bool _hasBalloon = true;
+    private Rigidbody2D _rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+
+        _rigidBody2D.mass = 15;
+        _rigidBody2D.gravityScale = 0;
+
+        OnEnemyKilled += Boom;
+        OnBallonDestroyed += BallDestroyed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Player pl = col.gameObject.GetComponent<Player>();
+
+            if (pl)
+            {
+                if (_hasBalloon)
+                {
+                }
+                else
+                {
+                    OnEnemyKilled();
+                }
+            }
+            else
+            {
+                Debug.LogWarning("There's no Player component on " + col.gameObject.name + " gameObject.");
+            }
+        }
+    }
+
+    private void BallDestroyed()
+    {
+        Debug.Log("Asu!");
+
+        _hasBalloon = false;
+        balloon.SetActive(false);
+
+        _hasBalloon = false;
+
+        _rigidBody2D.mass = 1;
+        _rigidBody2D.gravityScale = 1;
+    }
+
+    private void Boom()
+    {
+        // TODO: write something here when enemy killed...
+
+        Debug.Log("Boom!");
+    }
+
+    private void OnDestroy()
+    {
+        OnBallonDestroyed = null;
+        OnEnemyKilled = null;
+    }
+}
---
+++ Assets/Scripts/Model/EnemyBalloon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class EnemyBalloon : MonoBehaviour
+{
+    public Enemy enemyParent;
+
+    public UnityAction OnBalloonDestroyed = delegate { };
+
+    // Use this for initialization
+    void Start()
+    {
+        OnBalloonDestroyed += OnBalloonHit;
+        enemyParent.OnBallonDestroyed += OnBalloonDestroyed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("Balloon destroyed");
+            enemyParent.OnBallonDestroyed();
+        }
+    }
+
+    private void OnBalloonHit()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        enemyParent.OnBallonDestroyed -= OnBalloonDestroyed;
+        OnBalloonDestroyed = null;
+    }
+}
