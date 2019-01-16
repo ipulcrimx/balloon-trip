dc0584bbfc301044ef920996fff5eee923579253
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-01-17 00:17:59 +0700

    create player ballon script
    and move enemy ballon script to right folder

---
+++ Assets/Scripts/Model/Balloon/PlayerBalloon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class PlayerBalloon : MonoBehaviour
+{
+    public UnityAction OnBalloonPoppedUp;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
