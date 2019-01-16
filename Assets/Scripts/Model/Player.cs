1ee5a30a50dc948e108198431a352acac136ed7a
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-01-16 11:59:35 +0700

    create simple input for player

---
+++ Assets/Scripts/Model/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 0.15f;
+    public float jumpPower = 0.5f;
+
+    public GameObject[] balloons;
+
+    private int live;
+    private Rigidbody2D _rigidBody2d;
+
+    private void Awake()
+    {
+        live = Constant.startingLive;
+        _rigidBody2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveHorizontal(-moveSpeed);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveHorizontal(moveSpeed);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+#elif UNITY_ANDROID
+
+#endif
+
+    }
+
+    #region Input Methods
+    private void MoveHorizontal(float speed)
+    {
+        transform.position += new Vector3(speed * Time.deltaTime, 0);
+    }
+
+    private void Jump()
+    {
+        _rigidBody2d.AddForce(Vector2.up * jumpPower);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Utilities/Constant.cs
@@ -0,0 +1,8 @@
+﻿public class Constant
+{
+    public const int startingLive = 3;
+
+    public const int scorePerBalloon = 100;
+    public const int scoreFinishingHit = 500;
+    public const int scorePitFall = 1000;
+}
