bb9115ed4dba4d877cfcbf5bac8e6549207e4e80
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-01-27 22:25:54 +0700

    rework reset button
    instead of resetting only position, reset button will restart the game now

--- Assets/Scripts/Debug/ResetButton.cs
+++ Assets/Scripts/Debug/ResetButton.cs
@@ -1,46 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
 public class ResetButton : MonoBehaviour
 {
-    public Transform[] savedTransforms;
-
-    private Button _button;
-    private Vector3[] _savedPositions;
-
-#if DEBUG
-    void Start()
+    public void OnResetClicked()
     {
-        _savedPositions = new Vector3[savedTransforms.Length];
-
-        for(int i = 0; i < _savedPositions.Length;i++)
-        {
-            _savedPositions[i] = savedTransforms[i].position;
-        }
-
-        _button = GetComponent<Button>();
-        if(!_button)
-        {
-            Debug.LogError("There's no button component in it!");
-            return;
-        }
-
-        _button.onClick.AddListener(() => { OnButtonClicked(); });
+        SceneManager.LoadScene(0);
     }
-    public void OnButtonClicked()
-    {
-        for (int i = 0; i < _savedPositions.Length; i++)
-        {
-            savedTransforms[i].position = _savedPositions[i];
-        }
-    }
-#else
-    void Start()
-    {
-        gameObject.SetActive(false);
-    }
-#endif
 }
