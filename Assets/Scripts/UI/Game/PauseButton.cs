580444efb5e94d3654acd60f82dc33d80b98092c
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-02-01 12:18:21 +0700

    implement UI Scripts

--- Assets/Scripts/UI/Game/PauseButton.cs
+++ Assets/Scripts/UI/Game/PauseButton.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class PauseButton : MonoBehaviour
 {
+    public PausePopUpManager pausePopUp;
+    private Button _btn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        _btn = GetComponent<Button>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _btn.onClick.AddListener(() =>
+        {
+            if(SoundManager.instance) SoundManager.instance.PlaySFX();
+            pausePopUp.TogglePopUp();
+        });
     }
 }
--- Assets/Scripts/UI/Game/PostGamePopUpManager.cs
+++ Assets/Scripts/UI/Game/PostGamePopUpManager.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PostGamePopUpManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button restartButton;
+    public Button menuButton;
+    public Button quitButton;
+
+    private bool _isOpen = false;
+    private SoundManager _soundManager;
+
+    public void Init()
     {
-        
+        _soundManager = SoundManager.instance;
+
+        restartButton.onClick.AddListener(() => { OnRestartClicked(); });
+        menuButton.onClick.AddListener(() => { OnMenuClicked(); });
+        quitButton.onClick.AddListener(() => { OnQuitClicked(); });
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("Toggle Post Game")]
+    public void TogglePopUp()
     {
-        
+        _isOpen = !_isOpen;
+
+        Time.timeScale = _isOpen ? 0 : 1;
+        gameObject.SetActive(_isOpen);
+    }
+
+    private void OnRestartClicked()
+    {
+        if (_soundManager) _soundManager.PlaySFX();
+        SceneManager.LoadScene("Game");
+    }
+
+    private void OnMenuClicked()
+    {
+        if (_soundManager) _soundManager.PlaySFX();
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    private void OnQuitClicked()
+    {
+        if (_soundManager) _soundManager.PlaySFX();
+        Application.Quit();
     }
 }
