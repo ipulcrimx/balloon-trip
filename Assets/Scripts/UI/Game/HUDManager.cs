1466105b39da6d5fb5783e203b4acc47eddef2e7
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-02-07 16:25:51 +0700

    create HUD scirpts

---
+++ Assets/Scripts/UI/Game/HUDManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public Text coinText;
+    public Text scoreText;
+    public Text killText;
+
+    private GameManager _gameManager = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        _gameManager = GameManager.instance;
+
+        if(PlayerManager.instance) coinText.text = PlayerManager.instance.coin.ToString();
+        _gameManager.OnScoreChanged += OnScoreChanged;
+        _gameManager.OnKillChanged += OnKillValueChanged;
+    }
+
+    private void OnKillValueChanged()
+    {
+        killText.text = _gameManager.kill.ToString();
+    }
+
+    private void OnScoreChanged()
+    {
+        scoreText.text = _gameManager.score.ToString();
+    }
+
+    private void OnDestroy()
+    {
+        _gameManager.OnScoreChanged -= OnScoreChanged;
+        _gameManager.OnKillChanged -= OnKillValueChanged;
+    }
+}
