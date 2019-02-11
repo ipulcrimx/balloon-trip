65b212726105b4ee3db69eeec9ac67863c31c498
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-02-11 15:54:45 +0700

    create button group input

---
+++ Assets/Scripts/Input/ButtonInputGroup.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ButtonInputGroup : MonoBehaviour
+{
+    public string jump_key = "Jump";
+    public bool isInput { private set; get; }
+
+    CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // Reference to the joystick in the cross platform input
+
+    private void Awake()
+    {
+        isInput = false;
+    }
+
+    public void OnPointerEnterLeftButton()
+    {
+        if(isInput)
+        {
+            m_HorizontalVirtualAxis.Update(-1);
+        }
+    }
+
+    public void OnPointerExitLeftButton()
+    {
+
+    }
+
+    public void OnPointerEnterRightButton()
+    {
+        if(isInput)
+        {
+            m_HorizontalVirtualAxis.Update(1);
+        }
+    }
+
+    public void OnPointerExitRightButton()
+    {
+
+    }
+
+    public void OnJumpButtonUp()
+    {
+        CrossPlatformInputManager.SetButtonUp(jump_key);
+    }
+
+    public void OnJumButtonDown()
+    {
+        CrossPlatformInputManager.SetButtonDown(jump_key);
+    }
+
+    public void OnPointerDown()
+    {
+        isInput = true;
+    }
+
+    public void OnPointerUp()
+    {
+        isInput = false;
+    }
+}
