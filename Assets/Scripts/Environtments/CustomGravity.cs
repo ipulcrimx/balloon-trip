ebb3c1574e69f79e384d8d10a4bd6e88ea040e41
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-02-23 10:15:45 +0700

    create script for custom gravity

---
+++ Assets/Scripts/Environtments/CustomGravity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CustomGravity : MonoBehaviour
+{
+    public bool isAffected = true;
+    [Space]
+    public GravValueScriptableObject gravityValue;
+    public Transform planetTransform;
+
+    private Rigidbody2D _rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isAffected)
+        {
+            transform.up = (transform.position - planetTransform.position).normalized;
+
+            _rigidBody.AddForce(-transform.up * gravityValue.gravity * _rigidBody.mass);
+        }
+    }
+}
