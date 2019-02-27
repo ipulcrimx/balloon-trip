8acb0a472743ae84e8291feee76355632f35f890
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-02-27 10:51:08 +0700

    make new input

---
+++ Assets/Scripts/Managers/ControlManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ControlManager : MonoBehaviour
+{
+    [Range(0,1)]
+    public float minimalThreshold;
+    public float playerJumpPower;
+    public float maximumSpeed;
+    public float currentSpeed;
+    public Vector2 inputValue;
+    [Space]
+    [Range(0, 10)]
+    public float slowDownSpeed;
+
+    public Player player;
+    public Transform world;
+
+
+    private Joystick _joystick;
+    private bool _isInput;
+    
+    #region Instance
+    private static ControlManager _instance = null;
+
+    public static ControlManager instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                Debug.LogWarning("Instance haven't initiated yet....");
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        if(_instance)
+        {
+            Debug.LogWarning("Duplicate of ControlManager detected....\nDestroying itslef");
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _joystick = Joystick.instance;
+
+        if(InputSelector.instance)
+        {
+            if(InputSelector.instance.inputType != InputType.Button)
+            {
+                if (_joystick)
+                {
+                    _joystick.OnStartInput += delegate ()
+                    {
+                        _isInput = true;
+                    };
+
+                    _joystick.OnStopInput += delegate ()
+                    {
+                        _isInput = false;
+                    };
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        inputValue = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), 0);
+        bool isJump = CrossPlatformInputManager.GetButton("Jump");
+
+        if(inputValue.sqrMagnitude >= minimalThreshold)
+        {
+            RotateTheWorld();
+        }
+
+        if(isJump)
+        {
+            isJump = false;
+            PlayerJump();
+        }
+
+        if(!_isInput)
+        {
+            inputValue = Vector2.zero;
+            SlowTheWorldDown();
+        }
+    }
+
+    private void PlayerJump()
+    {
+        player.DoJump(playerJumpPower);
+    }
+
+    private void RotateTheWorld()
+    {
+        currentSpeed = Mathf.Lerp(currentSpeed, maximumSpeed * inputValue.x, Time.deltaTime);
+        world.Rotate(new Vector3(0, 0, 1), currentSpeed);
+    }
+
+    private void SlowTheWorldDown()
+    {
+        currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime / slowDownSpeed);
+        world.Rotate(new Vector3(0, 0, 1), currentSpeed);
+    }
+}
--- Assets/Scripts/Model/Player.cs
+++ Assets/Scripts/Model/Player.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 0.15f;
     public float jumpPower = 0.5f;
-    public float slowDownModifier = 2f;
-    [Space]
-    public float invincibleDuration = 1;
-
-    [Space]
-    public Vector2 thresholdSpeed;
-    public float pushDownForce =  2.5f;
-    public float maxHorizontalSpeed;
-    [Space]
     public List<GameObject> balloons = new List<GameObject>();
 
     private bool _isDead = false;
-    private bool _isInput = false;
-    private float _invinTimer = 0;
     private Rigidbody2D _rigidBody2d;
 
     public UnityAction OnBallonDestroyed = delegate { };
@@ -33,9 +19,9 @@ public class Player : MonoBehaviour
         get
         {
             int temp = 0;
-            foreach(Transform t in transform)
+            foreach (Transform t in transform)
             {
-                if(t.gameObject.activeInHierarchy)
+                if (t.gameObject.activeInHierarchy)
                 {
                     temp++;
                 }
@@ -51,23 +37,8 @@ public class Player : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    IEnumerator Start()
+    void Start()
     {
-        if (InputSelector.instance)
-        {
-            if (InputSelector.instance.inputType == InputType.Button)
-            {
-                yield return new WaitForEndOfFrame();
-            }
-            else
-            {
-                yield return new WaitUntil(() => Joystick.instance != null);
-
-                Joystick.instance.OnStartInput += StartInput;
-                Joystick.instance.OnStopInput += StopInput;
-            }
-        }
-
         OnPlayerHit += PlayerHit;
         OnBallonDestroyed += BallonHit;
     }
@@ -75,28 +46,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-#if UNITY_EDITOR
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            //_isInput = true;
-            MoveHorizontal(-moveSpeed);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            //_isInput = true;
-            MoveHorizontal(moveSpeed);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-#endif
-        if(_invinTimer < invincibleDuration)
-        {
-            _invinTimer +=Time.deltaTime;
-        }
-
         if (TotalBalloon <= 0 && !_isDead)
         {
             _isDead = true;
@@ -104,113 +53,47 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
-    {
-
-        if (!_isDead)
-        {
-            Vector2 move = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
-            bool isJump = CrossPlatformInputManager.GetButton("Jump");
-
-            if (move.sqrMagnitude >= 0.15f)
-            {
-                MoveHorizontal(move);
-            }
-
-            if (isJump)
-            {
-                isJump = false;
-                Jump();
-            }
-
-            if (!_isInput)
-            {
-                _rigidBody2d.velocity = Vector2.Lerp(_rigidBody2d.velocity, Vector2.zero, Time.deltaTime / slowDownModifier);
-            }
-        }
-
-        if(_rigidBody2d.velocity.x > maxHorizontalSpeed)
-        {
-            _rigidBody2d.velocity = new Vector2(maxHorizontalSpeed, _rigidBody2d.velocity.y);
-        }
-    }
-
-
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.tag == "Enemy" || col.gameObject.tag == "Enemies")
+        if (col.gameObject.tag == "Enemy" || col.gameObject.tag == "Enemies")
         {
             Enemy en = col.gameObject.GetComponent<Enemy>();
 
-            if(en)
+            if (en)
             {
-                if(en.hasBallon && _invinTimer >= invincibleDuration)
-                {
-                    OnPlayerHit();
-                }
+                OnPlayerHit();
             }
             else
             {
                 Debug.LogWarning("There's no enemy component on collided object!");
             }
         }
-        else if (col.gameObject.tag == "Interactable Wall")
-        {
-            if (_rigidBody2d.velocity.y < thresholdSpeed.y)
-            {
-                _rigidBody2d.velocity += Vector2.down *pushDownForce;
-            }
-        }
     }
 
-    #region Input Methods
-    private void MoveHorizontal(float speed)
+    public void DoJump(float jumpPower)
     {
-        //transform.position += new Vector3(speed * Time.deltaTime, 0);
-        _rigidBody2d.AddForce(Vector2.right*speed);
-    }
-
-    private void MoveHorizontal(Vector2 direction)
-    {
-        //transform.position += moveSpeed * Time.deltaTime * (Vector3)direction;
-        _rigidBody2d.AddForce(direction * moveSpeed);
-        //speed = _rigidBody2d.velocity;
-        //magnitude = speed.magnitude;
-    }
-
-    private void Jump()
-    {
-        //_rigidBody2d.velocity = Vector2.zero;
         _rigidBody2d.AddForce(Vector2.up * jumpPower);
-        //speed = _rigidBody2d.velocity;
-        //magnitude = speed.magnitude;
     }
-    #endregion
 
     private void BallonHit()
     {
-        if (_invinTimer < invincibleDuration)
-            return;
-
         int index = 0;
         GameObject bal = null;
-        for(index = 0; index < balloons.Count;index++)
+        for (index = 0; index < balloons.Count; index++)
         {
-            if(balloons[index].activeInHierarchy)
+            if (balloons[index].activeInHierarchy)
             {
                 bal = balloons[index];
                 break;
             }
         }
 
-        if(bal)
+        if (bal)
         {
             Debug.Log("Index: " + index);
             bal.transform.SetParent(null);
             balloons.RemoveAt(index);
             bal.SetActive(false);
-
-            _invinTimer = 0;
         }
         else
         {
@@ -218,22 +101,9 @@ public class Player : MonoBehaviour
         }
     }
 
-    public void StartInput()
-    {
-        _isInput = true;
-    }
-
-    public void StopInput()
-    {
-        _isInput = false;
-    }
-
     [ContextMenu("Player Hit")]
     private void PlayerHit()
     {
-        if (_invinTimer < invincibleDuration)
-            return;
-
         OnBallonDestroyed();
 
         if (TotalBalloon <= 0)
