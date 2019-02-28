9c90282eda654ca6769ff855787adf8e9bbc0e7e
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-02-28 13:31:59 +0700

    remove black hole area script

--- Assets/Scripts/Model/Enemies/Black Holes/BlackHole.cs
+++ Assets/Scripts/Model/Enemies/Black Holes/BlackHole.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class BlackHole : MonoBehaviour
 {
+    public float moveSpeed;
+    [Space]
+    public float suckArea = 15f;
+    public float suckPower;
+
+    private GameManager _gameManager;
+    private AsteroidManager _asteroidManager;
+
+    private Player _player;
+    private Alien[] _aliens;
+    private GameObject[] _asteroids
+    {
+        get
+        {
+            return _asteroidManager.asteroids;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float move = CrossPlatformInputManager.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * move * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+        }
+        else if (col.gameObject.tag == "Enemy" || col.gameObject.tag == "Enemies")
+        {
+
+        }
+        else if (col.gameObject.tag == "Asteroid")
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
