25194173dcea65325e81b3f37f4a8e283fe44cef
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-03-04 11:56:11 +0700

    make blackhole will suck stronger the longer the object is still

--- Assets/Scripts/Model/Enemies/Black Holes/BlackHole.cs
+++ Assets/Scripts/Model/Enemies/Black Holes/BlackHole.cs
@@ -10,13 +10,14 @@ public class BlackHole : MonoBehaviour
     [Header("For Player Only")]
     public float[] distanceLevels;
 
+    private float _currentPower = 0;
     private GameManager _gameManager;
     private AsteroidManager _asteroidManager;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _currentPower = suckPower;
     }
 
     // Update is called once per frame
@@ -30,25 +31,26 @@ public class BlackHole : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
+            _currentPower += Time.deltaTime;
             Transform tr = col.transform;
             Player pl = tr.GetComponent<Player>();
 
             pl.OnEnterBlackHole();
             if (pl.distanceFromInitialPosition < distanceLevels[0])
             {
-                tr.position = Vector3.Lerp(tr.position, (Vector2)tr.position + GetDirection(tr.position), Time.deltaTime * suckPower);
+                tr.position = Vector3.Lerp(tr.position, (Vector2)tr.position + GetDirection(tr.position), Time.deltaTime * _currentPower);
             }
             else if (pl.distanceFromInitialPosition < distanceLevels[1])
             {
-                tr.position = Vector3.Lerp(tr.position, (Vector2)tr.position + GetDirection(tr.position), Time.deltaTime * suckPower / 3);
+                tr.position = Vector3.Lerp(tr.position, (Vector2)tr.position + GetDirection(tr.position), Time.deltaTime * _currentPower / 3);
             }
             else if (pl.distanceFromInitialPosition < distanceLevels[2])
             {
-                tr.position = Vector3.Lerp(tr.position, (Vector2)tr.position + GetDirection(tr.position), Time.deltaTime * suckPower / 5);
+                tr.position = Vector3.Lerp(tr.position, (Vector2)tr.position + GetDirection(tr.position), Time.deltaTime * _currentPower / 5);
             }
             else
             {
-                tr.position = Vector3.Lerp(tr.position, (Vector2)tr.position + GetDirection(tr.position), Time.deltaTime * suckPower / 30);
+                tr.position = Vector3.Lerp(tr.position, (Vector2)tr.position + GetDirection(tr.position), Time.deltaTime * _currentPower / 30);
             }
 
             tr.GetComponent<CustomGravity>().isDisturbed = true;
@@ -60,13 +62,15 @@ public class BlackHole : MonoBehaviour
         }
         else if (col.gameObject.tag == "Enemy" || col.gameObject.tag == "Enemies")
         {
+            _currentPower += Time.deltaTime;
             Transform tr = col.transform;
             tr.position = Vector3.Lerp(tr.position, transform.position, Time.deltaTime * suckPower);
         }
         else if (col.gameObject.tag == "Asteroid")
         {
+            _currentPower += Time.deltaTime;
             Transform tr = col.transform;
-            tr.position = Vector3.Lerp(tr.position, transform.position, (Time.deltaTime * 3) * suckPower);
+            tr.position = Vector3.Lerp(tr.position, transform.position, (Time.deltaTime * 3) * _currentPower);
 
             if (Vector2.Distance(tr.position, transform.position) <= damageArea)
             {
@@ -90,6 +94,8 @@ public class BlackHole : MonoBehaviour
             col.gameObject.GetComponent<CustomGravity>().isDisturbed = false;
             col.gameObject.GetComponent<Player>().OnExitBlackHole();
         }
+
+        _currentPower = suckPower;
     }
 
     private Vector2 GetDirection(Vector2 startPos)
