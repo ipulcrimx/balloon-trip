bcd78fbf7e5b2b18987d8738a56e8a92e114f7f5
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-03-16 23:01:11 +0700

    make warning countdown timer script

---
+++ Assets/Scripts/UI/Game/WarningCountdown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarningCountdown : MonoBehaviour
+{
+    public string suffixText = " seconds more, you ballon will popped";
+    public Text countDownText;
+
+    private bool _isOpen = false;
+
+    public void Init()
+    {
+        _isOpen = false;
+        gameObject.SetActive(false);
+    }
+
+
+    public void SetCountdown(float sec)
+    {
+        countDownText.text = sec.ToString("0.0") + suffixText;
+    }
+
+    public void SetCountdown(string sec)
+    {
+        countDownText.text = sec + suffixText;
+    }
+
+    public void ToggleWarning()
+    {
+        _isOpen = !_isOpen;
+        gameObject.SetActive(_isOpen);
+    }
+}
